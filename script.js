b819d9b783a5b1ca30db829243e2ba513c610902
umarahgul <umarah.gul@gmail.com>
2023-08-01 02:58:07 +0500

    created mobile menu on clicking hamburger menu

---
+++ script.js
@@ -0,0 +1,5 @@
+function openNav() {
+    document.getElementById('hamburgerMenu').style.display = 'block';}
+
+    function closeNav() {
+        document.getElementById('hamburgerMenu').style.display = 'none';} 
